f8f8701f0d374ed710dc25846e0ebf1e0d0aef10
SorenAndersen1 <47064192+SorenAndersen1@users.noreply.github.com>
2021-07-22 17:36:26 -0700

    Inserted file

---
+++ look_and_say.cc
@@ -0,0 +1,51 @@
+#include <string>
+
+#include "test_framework/generic_test.h"
+using std::string;
+
+string LookAndSay(int n) {
+  // TODO - you fill in here.
+	std::vector<std::string> lookArr;
+	lookArr.push_back("1");
+	
+	for (int i = 0; i < n - 1; i++)
+	{
+		std::string constructString;
+		std::string holder = lookArr.at(i);
+		int len = holder.length();
+		char currentStringNum;
+		int consectChars = 0;
+		if (holder.length() > 1) 
+		{
+				for (int j = 0; j < len - 1; j)
+				{
+					currentStringNum = holder[j];
+					int thismany = 1;
+					while (currentStringNum == holder[j + thismany])
+						{
+							thismany++;
+						}
+					char complex = thismany + 64;
+					std::string thismanyString(1, complex);
+					constructString = constructString + thismanyString + holder[j];
+					j = j + thismany;
+
+				}
+				lookArr.push_back(constructString);
+		}
+		else {
+			lookArr.push_back("1" + holder[i]);
+			
+		}
+
+		
+	}
+  return lookArr.at(n - 1);
+}
+
+int main(int argc, char* argv[]) {
+  std::vector<std::string> args{argv + 1, argv + argc};
+  std::vector<std::string> param_names{"n"};
+  return GenericTestMain(args, "look_and_say.cc", "look_and_say.tsv",
+                         &LookAndSay, DefaultComparator{}, param_names);
+}
